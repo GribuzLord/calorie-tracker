4866f281b523ac964e3eb82e337ca0563574261c
GribuzLord <gribuzcode@gmail.com>
2025-07-30 17:47:35 -0600

    Se agregó el hook de estado a el formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 
 export default function Form() {
+  const [activity,setActivity]=useState({
+    category:1,
+    name:'',
+    calories:0
+  })
+
+
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg" >
 
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">Categoría:</label>
-        <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category">
+        <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category"              value={activity.category}>
           {categories.map(category=>(
             <option 
             key={category.id}
@@ -18,14 +27,14 @@ export default function Form() {
         </select>
       </div>
       <div className="grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">Actividad:</label>
-        <input id="activity" type="text" className="border border-slate-300 p-2 rounded-lg"
-        placeholder="Ej. Comida, jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta" />
+        <label htmlFor="name" className="font-bold">Actividad:</label>
+        <input id="name" type="text" className="border border-slate-300 p-2 rounded-lg"
+        placeholder="Ej. Comida, jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta" value={activity.name} />
       </div>
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="calories" className="font-bold">Calorias:</label>
         <input id="calories" type="number" className="border border-slate-300 p-2 rounded-lg"
-        placeholder="Calorias. Ej. 300 o 500" />
+        placeholder="Calorias. Ej. 300 o 500" value={activity.calories}/>
       </div>
       <input type="submit" className="bg-gray-800 hober: bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" value='Guardar Comida o Guardar Ejercicio' />
     </form>
