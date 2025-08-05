0c919477d1e068b5df6c468f8fbbf534ed98ffd6
GribuzLord <gribuzcode@gmail.com>
2025-08-04 19:54:04 -0600

    Se creo el type actividad y se enviaron los valores correctamente al state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,25 @@
 import { useState} from "react"
 import type{ ChangeEvent } from "react"
+import type{ Activity } from "../types"
 import { categories } from "../data/categories"
 
 
 export default function Form() {
-  const [activity,setActivity]=useState({
+  const [activity,setActivity]=useState<Activity>({
     category:1,
     name:'',
     calories:0
   })
 
 const handleChange=(e:ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>)=>{
+
+  const isNumberField=['category','calories'].includes(e.target.id)
+  console.log(isNumberField)
+
+
   setActivity({
     ...activity,
-    [e.target.id]:e.target.value
+    [e.target.id]:isNumberField ? +e.target.value:e.target.value
   })
   console.log(e.target.id)
   console.log(e.target.value)
